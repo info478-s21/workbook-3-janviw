127cc1617958eb5d6833ea0444cc1646feadfbba
janviw <janviw@uw.edu>
2021-05-03 13:07:06 -0700

    questions answered

--- analysis.R
+++ analysis.R
@@ -6,16 +6,38 @@ library(dplyr)
 library(ggplot2)
 
 # Create the `charts/` directory (you can do this from R!)
-
+dir.create("charts", showWarnings = FALSE)
 # Load prepped data
-
+health_data <- read.csv("./data/prepped/all_data.csv")
 # Are HALE and life expectancy correlated?
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
+data_2016 <- health_data %>% 
+  filter(year == 2016)
+
+ggplot(data_2016) +
+  geom_point(mapping = aes(x = le, y = hale)) +
+  labs( title = "Life Expectancy vs HALE", 
+        x = "Life Expectancy",
+        y = "HALE")
+ggsave("charts/le_hale_graph.png")
+cor(data_2016$hale, data_2016$le)
+
+#The correlation is strong and positive 
 
 # Are HALE and DALYs correlated?
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
+
+ggplot(data_2016) +
+  geom_point(mapping = aes(x = dalys, y = hale)) +
+  labs( title = "DALYs vs HALE", 
+        x = "DALYs",
+        y = "HALE")
+ggsave("charts/dalys_hale_graph.png")
+
+cor(data_2016$hale, data_2016$dalys)
+#strong negative correlation
 
 
 # As people live longer, do they live healthier lives 
@@ -27,10 +49,31 @@ library(ggplot2)
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
 
+data_wider <- health_data %>% 
+  pivot_wider(names_from = year,
+              values_from = c(hale, le, dalys))
 
 # Create columns to store the change in life expectancy, and change in hale
-
+data_wider <- data_wider %>% 
+  mutate(hale_diff = hale_2016 - hale_1990,
+         le_diff = le_2016 - le_1990)
 
 # Plot the *change in hale* against the *change in life expectancy*
 # Add a 45 degree line (i.e., where x = y), and save the graph to `charts/`
 # What does this mean?!?! Put your interpretation below
+
+ggplot(data_wider) +
+  geom_point(mapping = aes(x = le_diff, y = hale_diff )) +
+  labs (title = "Life Expectancy Difference vs HALE Difference" ,
+          x = "Change in Life Expectancy ", 
+          y = "Change in HALE") +
+  geom_abline(intercept = 0, slope = 1) +
+  xlim(-15, 20) +
+  ylim(-15, 20)
+ggsave("charts/difference_graph.png")
+
+# this graph shows us a trend that HALEs and life expectancy both have increased 
+# in most countries from 1990 to 2016. The positive correlation suggest as life expectancy 
+# increases so does HALE which supports the idea that as people live longer they live 
+# healthier lives.
+
